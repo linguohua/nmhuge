523f58e449f29709c1ac5d904e1cd2a3cae5d1d1
linguohua <lghchinaidea@163.com>
2021-09-06 10:04:37 +0800

    1gb

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ::libc::{*};
 use log::{error, info};
-use std::io::{Error, ErrorKind};
+use std::io::{Error, ErrorKind, Result};
 use simple_logger::SimpleLogger;
 
 // const MPOL_DEFAULT:c_int = 0;
@@ -40,6 +40,32 @@ void *mmap(void *addr, size_t length, int prot, int flags,
     ) -> c_long;
 }
 
+pub unsafe fn map_1gb(start_addr: * mut c_void, numa_node: u32) -> Result<*mut c_void> {
+    let gb = 1;
+    let protect_flags = PROT_READ|PROT_WRITE;
+    let flags = MAP_PRIVATE | MAP_ANONYMOUS|MAP_HUGE_1GB|MAP_HUGETLB;
+
+    let start_ptr = start_addr as *mut c_ulonglong as *mut c_void;
+    let addr = mmap(start_ptr, gb*GB,
+        protect_flags, flags, -1, 0);
+    if addr as c_ulonglong == 0 {
+        let e = format!("mmap failed:{}", Error::last_os_error());
+        return Err(Error::new(ErrorKind::Other, e));
+    }
+
+    let nmask = 1 << numa_node;
+    let mode = MPOL_USED;
+    let bflags = 0;
+    let result = mbind(addr, (gb*GB) as c_ulong, mode,
+    &nmask, 32, bflags);
+    if result != 0 {
+        let e = format!("mbind failed:{}", Error::last_os_error());
+        return Err(Error::new(ErrorKind::Other, e));
+    }
+
+    Ok(addr)
+}
+
 fn main() -> std::io::Result<()> {
     SimpleLogger::new().init().unwrap();
     // unsafe {
@@ -53,38 +79,24 @@ fn main() -> std::io::Result<()> {
     // }
 
     unsafe {
-        let gb = 8;
-        let protect_flags = PROT_READ|PROT_WRITE;
-        let flags = MAP_PRIVATE | MAP_ANONYMOUS|MAP_HUGE_1GB;
         let start_addr:c_ulonglong = 32 << 40; // 32TB
         let start_ptr = start_addr as *mut c_ulonglong as *mut c_void;
-        let addr = mmap(start_ptr, gb*GB,
-            protect_flags, flags, -1, 0);
-        if addr as c_ulonglong == 0 {
-            error!("mmap failed:{}", Error::last_os_error());
-            return Err(Error::new(ErrorKind::Other, "mmap failed"));
+        match map_1gb(start_ptr, 0) {
+            Ok(addr) => {
+                let gb = 1;
+                let ptr = addr as c_ulonglong as *mut u64;
+                // for _i in 1..gb {
+                //     *ptr = 0x1;
+                //     ptr = ptr.add(GB / std::mem::size_of::<u32>());
+                // }
+                std::ptr::write_bytes(ptr, 0, gb*GB/std::mem::size_of::<u64>());
+
+                info!("write to {} GB ok", gb);
+            },
+            Err(e) => {
+                error!("map_1gb failed:{}", e);
+            },
         }
-
-        info!("mmap ok, got: {}", gb);
-        let nmask = 0x01;
-        let mode = MPOL_USED;
-        let bflags = 0;
-        let result = mbind(addr, (gb*GB) as c_ulong, mode,
-        &nmask, 32, bflags);
-        if result != 0 {
-            error!("mbind failed:{}", Error::last_os_error());
-            return Err(Error::new(ErrorKind::Other, "mbind failed"));
-        }
-        info!("mbind ok");
-
-        let ptr = addr as c_ulonglong as *mut u64;
-        // for _i in 1..gb {
-        //     *ptr = 0x1;
-        //     ptr = ptr.add(GB / std::mem::size_of::<u32>());
-        // }
-        std::ptr::write_bytes(ptr, 0, gb*GB);
-
-        info!("write to {} GB ok", gb);
     }
 
     let mut buffer = String::new();
